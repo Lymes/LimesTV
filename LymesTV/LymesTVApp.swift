72e91ae80d5831423d28d96f73530fdcb559b832
leonid.mesentsev <leonid.mesentsev@accenture.com>
2026-07-19 10:17:29 +0200

    Initial Commit

---
+++ LymesTV/LymesTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  LymesTVApp.swift
+//  LymesTV
+//
+//  Created by leonid.mesentsev on 19/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct LymesTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
